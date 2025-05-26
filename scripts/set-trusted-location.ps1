716a7e23f23fe7ef87f4438233f596d8210caca5
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2025-05-27 01:48:30 +0200

    Create set-trusted-location.ps1

---
+++ scripts/set-trusted-location.ps1
@@ -0,0 +1,16 @@
+param(
+    [string]$LocationName
+    [string]$TrustedPath
+)
+
+$accessVersion = "16.0"  # O365
+
+# Registry
+$regPath = "HKCU:\Software\Microsoft\Office\$accessVersion\Access\Security\Trusted Locations\$LocationName"
+
+If (-Not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $regPath -Name "Path" -Value $TrustedPath -PropertyType String -Force
+New-ItemProperty -Path $regPath -Name "AllowSubfolders" -Value 1 -PropertyType DWord -Force
