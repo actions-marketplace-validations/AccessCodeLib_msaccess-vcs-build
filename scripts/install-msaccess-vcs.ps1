77d5699b6bddab453de183440a9c1b4f0ca7fb33
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2025-05-26 20:20:07 +0200

    Create install-msaccess-vcs.ps1

---
+++ scripts/install-msaccess-vcs.ps1
@@ -0,0 +1,35 @@
+# install msaccess-vcs
+#
+# URL
+$vcsUrl = "https://api.github.com/repos/joyfullservice/msaccess-vcs-addin/releases/latest"
+$headers = @{
+    "User-Agent" = "PowerShell"
+}
+$release = Invoke-RestMethod -Uri $vcsUrl -Headers $headers
+
+# zip url
+$asset = $release.assets | Where-Object { $_.name -like "*.zip" } | Select-Object -First 1
+$zipUrl = $asset.browser_download_url
+
+# save as
+$zipFile = "msaccess-vcs.zip"
+
+# download file
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
+
+Write-Host "zip file downloaded from $zipUrl to $zipFile"
+
+# extrat to %appdata%\Microsoft\AddIns
+$appdata = $env:APPDATA
+$addInFolder = Join-Path $appdata "Microsoft\AddIns"
+Expand-Archive -Path $zipFile -DestinationPath $addInFolder -Force
+
+$addInFileName = "msaccess-vcs.accda"
+$addInPath = Join-Path $addInFolder $addInFileName
+$unzipVcsFile = Join-Path $addInFolder "Version Control.accda"
+if (Test-Path $addInPath) {
+    Remove-Item $addInPath -Force
+}
+Rename-Item -Path $unzipVcsFile -NewName $addInFileName -Force
+
+Write-Host "msaccess-vcs installed: $addInPath"
