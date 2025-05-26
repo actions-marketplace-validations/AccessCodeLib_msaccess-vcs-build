c7ce80d86359c86fdcc1dfad7e50d3cd5eb3ec67
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2025-05-26 20:55:40 +0200

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,1 +1,8 @@
+param(
+    [string]$SourceDir = "source",
+    [string]$Compile = "false" # Default to "false" if not specified
+)
 
+Write-Host "Install msaccess-vcs"
+. "$PSScriptRoot/scripts/install-msaccess-vcs.ps1"
+Write-Host "-----"
