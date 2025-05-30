b5fae283bd5ccdb4add0c4b24804c1a4a80e4ec6
Josef Pötzl <115746022+josef-poetzl@users.noreply.github.com>
2025-05-30 13:55:19 +0200

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -4,10 +4,10 @@ param(
     [string]$vcsUrl = "https://api.github.com/repos/joyfullservice/msaccess-vcs-addin/releases/latest"
 )
 
-Write-Host "Install msaccess-vcs"
+Write-Host "Install msaccess-vcs" -ForegroundColor Blue
 . "$PSScriptRoot/scripts/install-msaccess-vcs.ps1" "${vcsUrl}"
 Write-Host "-----"
 
-Write-Host "build accda"
+Write-Host "Build accda" -ForegroundColor Blue
 . "$PSScriptRoot/scripts/build-accdb.ps1" "${SourceDir}"
 Write-Host "-----"
